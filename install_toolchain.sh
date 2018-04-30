336ac5f88c35f40c779861774c663c0f108e1823
Alex Balan <ammaaim@mail.com>
2018-04-30 20:51:34 +0300

    Create install_toolchain.sh

---
+++ install_toolchain.sh
@@ -0,0 +1,9 @@
+
+apt-get update
+apt-get -y install gcc-avr
+apt-get -y install binutils-avr
+apt-get -y install gdb-avr
+apt-get -y install avr-libc
+apt-get -y install avrdude
+apt-get -y install minicom
+apt-get -y install vim
